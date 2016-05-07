dddf5a117820d7d7b0ac481083a97d80283e7a4e
GuusHamm <guushamm@gmail.com>
2016-05-07 13:13:10 +0200

    Vraag 4 done

--- src/test/java/Vraag4.java
+++ src/test/java/Vraag4.java
@@ -16,7 +16,7 @@ import static org.junit.Assert.assertEquals;
  */
 public class Vraag4 {
 	/*
-    Voor elke test moet je in ieder geval de volgende vragen beantwoorden:
+	Voor elke test moet je in ieder geval de volgende vragen beantwoorden:
         Wat is de waarde van asserties en printstatements?
             Corrigeer verkeerde asserties zodat de test ‘groen’ wordt.
         Welke SQL statements worden gegenereerd?
@@ -58,14 +58,25 @@ public class Vraag4 {
 		account.setBalance(expectedBalance);
 		em.getTransaction().commit();
 		assertEquals(expectedBalance, account.getBalance());
-//TODO: verklaar de waarde van account.getBalance
+//		account.getBalance zou de waarde van 400 moeten hebben. dit is namelijk de waarde die we er aan hebben gegeven op regel 58
 		Long acId = account.getId();
+
 		account = null;
 		EntityManager em2 = emf.createEntityManager();
 		em2.getTransaction().begin();
 		Account found = em2.find(Account.class, acId);
-//TODO: verklaar de waarde van found.getBalance
+//		account.getBalance() zou dezelfde waarde moeten hebben als we op regel 58 hebben geset omdat we de account hebben opgeslagen en vervolgens uitlezen wat de opgeslagen waarde is.
 		assertEquals(expectedBalance, found.getBalance());
 
+		//2.	Welke SQL statements worden gegenereerd?
+		//      INSERT INTO ACCOUNT (ACCOUNTNR, BALANCE, THRESHOLD) VALUES (?, ?, ?);
+//		select lastval();
+//		SELECT ID, ACCOUNTNR, BALANCE, THRESHOLD FROM ACCOUNT WHERE (ID = ?);
+
+		//3.	Wat is het eindresultaat in de database?
+		//      1 Account in de database met account nummer 114, balance 400 & threshold 0.
+
+		//4.	Verklaring van bovenstaande drie observaties.
+		//      Er wordt een account gemaakt en deze wordt vervolgens opgeslagen, deze account komt dus in de database. Vervolgens wordt de lokale variabele veranderd in de database is deze echter nog hetzelfde met als gevolg dat zodra we uit de database de waarde opvragen deze de orginele waarde is
 	}
 }
