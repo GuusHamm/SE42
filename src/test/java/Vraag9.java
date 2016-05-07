ff9b46d62e16cae3ffe58e5bb5082509becb367a
binq1000 <t.daniels@student.fontys.nl>
2016-05-07 14:45:52 +0200

    Vraag 9 + databaseCleaner aangepast zodat hij ook de nieuwe klassen schoonmaakt.

--- src/test/java/Vraag9.java
+++ src/test/java/Vraag9.java
@@ -1,3 +1,6 @@
+import bank.domain.Account;
+import bank.domain.AccountVoorVraag9;
+import bank.domain.AccountVoorVraag9Table;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,6 +10,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.sql.SQLException;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Nekkyou on 25-4-2016.
@@ -53,5 +60,95 @@ public class Vraag9 {
 		Voer dezelfde opdracht nu uit met GenerationType SEQUENCE en TABLE.
 		Verklaar zowel de verschillen in testresultaat als verschillen van de database structuur.
 		 */
+
+		//region vraag 1
+
+		//Vraag 1:
+		int pointlessnumber = 0; // To get rid of the double code lines
+
+		Account account = new Account(111L);
+        em.getTransaction().begin();
+        em.persist(account);
+
+        //Voordat de transactie gecommit is, wordt er nog geen id aangemaakt. Wat wel aangemaakt is in een account nummer.
+        //De id is puur voor de database.
+        assertNull(account.getId());
+        em.getTransaction().commit();
+        System.out.println("AccountId: " + account.getId());
+        //Nadat em.getTransaction().commit() is aangeroepen zet hij de data van de transactie in de database en maakt er ook een id voor, Daarom is id niet meer null
+        assertTrue(account.getId() > 0L);
+
+        //2.	Welke SQL statements worden gegenereerd?
+        //      Er wordt een create table account gemaakt en een insert in het account
+        //      INSERT INTO ACCOUNT (ACCOUNTNR, BALANCE, THRESHOLD) VALUES (?, ?, ?)
+
+        //3.	Wat is het eindresultaat in de database?
+        //      1 Account in de database met id : 7, account nummer 111 en balance en threshold 0.
+
+        //4.	Verklaring van bovenstaande drie observaties.
+        //      Het ID wordt pas aangemaakt bij het committen, waardoor hij bij de eerste assertie null was, en bij de 2de niet meer.
+        //      Na de commit is het account ook in de database gezet met de waardes.
+		//endregion
+	}
+
+	@Test
+	public void GenerationTypeTestSequence() {
+		//region met Sequence
+		AccountVoorVraag9 account = new AccountVoorVraag9(114L);
+		em.getTransaction().begin();
+		em.persist(account);
+
+		//Voordat de transactie gecommit is, wordt er nog geen id aangemaakt. Wat wel aangemaakt is in een account nummer.
+		//De id is puur voor de database.
+//		assertNull(account.getId());
+		assertNotNull(account.getId()); // Nu we met Sequence werken is er blijkbaar al wel een id
+		em.getTransaction().commit();
+		System.out.println("AccountId: " + account.getId());
+		//Nadat em.getTransaction().commit() is aangeroepen zet hij de data van de transactie in de database en maakt er ook een id voor, Daarom is id niet meer null
+		assertTrue(account.getId() > 0L);
+
+		//2.	Welke SQL statements worden gegenereerd?
+		//      Er wordt een create table account gemaakt en een insert in het account
+		//      INSERT INTO ACCOUNTVOORVRAAG9 (ID, ACCOUNTNR, BALANCE, THRESHOLD) VALUES (?, ?, ?, ?)
+		//		SELECT * FROM SEQUENCE WHERE SEQ_NAME = SEQ_GEN_TABLE
+		//		select nextval(SEQ_GEN_SEQUENCE)
+		//		ALTER SEQUENCE SEQ_GEN_SEQUENCE INCREMENT BY 50
+
+		//3.	Wat is het eindresultaat in de database?
+		//      1 Account in de database die gebruik maakt van de Sequence.
+
+		//4.	Verklaring van bovenstaande drie observaties.
+		//      Het maakt nu gebruik van Sequence i.p.v Identity
+
+		//endregion
+	}
+
+	@Test
+	public void GenerationTypeTestTable() {
+
+		//region met Table
+		AccountVoorVraag9Table accountTable = new AccountVoorVraag9Table(115L);
+		em.getTransaction().begin();
+		em.persist(accountTable);
+
+		//Voordat de transactie gecommit is, wordt er nog geen id aangemaakt. Wat wel aangemaakt is in een account nummer.
+		//De id is puur voor de database.
+//		assertNull(accountTable.getId());
+		em.getTransaction().commit();
+		System.out.println("AccountId: " + accountTable.getId());
+		//Nadat em.getTransaction().commit() is aangeroepen zet hij de data van de transactie in de database en maakt er ook een id voor, Daarom is id niet meer null
+		assertTrue(accountTable.getId() > 0L);
+
+		//2.	Welke SQL statements worden gegenereerd?
+		//      UPDATE SEQUENCE SET SEQ_COUNT = SEQ_COUNT + ? WHERE SEQ_NAME = ?
+		//		SELECT SEQ_COUNT FROM SEQUENCE WHERE SEQ_NAME = ?
+		//		INSERT INTO ACCOUNTVOORVRAAG9TABLE (ID, ACCOUNTNR, BALANCE, THRESHOLD) VALUES (?, ?, ?, ?)
+
+		//3.	Wat is het eindresultaat in de database?
+		//      1 Account in de database die gebruik maakt van de Sequence. terwijl je iets van Table zou verwachten
+
+		//4.	Verklaring van bovenstaande drie observaties.
+		//      Het maakt nu gebruik van Table i.p.v Identity of Sequence
+//endregion
 	}
 }
